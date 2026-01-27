3ff127dee2b07c2d8dad99aea39aa819e095fd84
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-27 23:48:42 +0530

    Login Page

---
+++ src/main/java/org/OpenCartLoginPage.java
@@ -0,0 +1,15 @@
+package org;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class OpenCartLoginPage {
+    private WebDriver driver;
+    public OpenCartLoginPage(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+
+
+}
