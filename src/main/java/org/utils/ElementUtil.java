ae376dd9233e9f7d3d52b328a141e298d6059ecd
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-02 23:49:22 +0530

    Element util update

--- src/main/java/org/utils/ElementUtil.java
+++ src/main/java/org/utils/ElementUtil.java
@@ -13,11 +13,41 @@ public class ElementUtil {
 
     }
 
+    public By getBy(String locatorType, String locatorValue)
+    {
+        By locator=null;
+
+        switch (locatorType.trim().toLowerCase())
+        {
+            case "id": locator = By.id(locatorValue);
+            break;
+
+            case "name": locator = By.name(locatorValue);
+            break;
+
+            case "classname": locator = By.className(locatorValue);
+            break;
+
+            default: break;
+         }
+    return locator;
+    }
+
+    public WebElement getElement(String locatorType, String locatorValue)
+    {
+        return getElement(getBy(locatorType,locatorValue));
+    }
+
     public WebElement getElement(By locator)
     {
         return driver.findElement(locator);
     }
 
+    public void doSendKeys(String locatorType, String locatorValue, String value)
+    {
+        doSendKeys(getBy(locatorType,locatorValue),value);
+     //   getElement(locatorType,locatorValue).sendKeys(value);
+    }
     public void doSendKeys(By locator, String value)
     {
         getElement(locator).sendKeys(value);
