340661234a26823b6d5bf00dfed6dddbf180eea8
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-12 08:58:03 +0530

    Get User names

--- src/test/java/org/example/SelectorsHUBPractice.java
+++ src/test/java/org/example/SelectorsHUBPractice.java
@@ -13,11 +13,12 @@ import org.utils.BrowserUtil;
 import org.utils.ElementUtil;
 
 import java.time.Duration;
+import java.util.List;
 
 public class SelectorsHUBPractice {
     static WebDriver driver;
 
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException {
         BrowserUtil browserUtil = new BrowserUtil();
 
         driver = browserUtil.launchBrowser("chrome");
@@ -25,23 +26,23 @@ public class SelectorsHUBPractice {
 
         browserUtil.launchUrl("https://selectorshub.com/xpath-practice-page/");
 
-        if(elementUtil.isPageLoaded(3)) {
-            By userEmail = By.name("email");
-            By password = By.id("pass");
-            By company = By.name("company");
-            By mobileNumber = By.name("mobile number");
-            By country = By.xpath("//label[text()='Country']/input");
-            //  By submit = By.xpath("//button[@value = 'Submit']");
-            By firstCrush = By.cssSelector("#inp_val");
-            //By downloadLink = By.linkText("Download Link");
-          //  By downloadLink = By.xpath("//*[@id='content']/div[2]/div/div[2]/div/div[1]/div[2]/div/div/span/a");
-
-            elementUtil.doSendKeys(userEmail, "abc@gmail.com");
-            elementUtil.doSendKeys(password, "#1243@Test");
-            elementUtil.doSendKeys(company, "CTS");
-            elementUtil.doSendKeys(mobileNumber, "342423423");
-            elementUtil.doSendKeys(country, "India");
-            elementUtil.doSendKeys(firstCrush,"Hoor");
+        if (elementUtil.isPageLoaded(3)) {
+//            By userEmail = By.name("email");
+//            By password = By.id("pass");
+//            By company = By.name("company");
+//            By mobileNumber = By.name("mobile number");
+//            By country = By.xpath("//label[text()='Country']/input");
+//            //  By submit = By.xpath("//button[@value = 'Submit']");
+//            By firstCrush = By.cssSelector("#inp_val");
+//            //By downloadLink = By.linkText("Download Link");
+//            //  By downloadLink = By.xpath("//*[@id='content']/div[2]/div/div[2]/div/div[1]/div[2]/div/div/span/a");
+//
+//            elementUtil.doSendKeys(userEmail, "abc@gmail.com");
+//            elementUtil.doSendKeys(password, "#1243@Test");
+//            elementUtil.doSendKeys(company, "CTS");
+//            elementUtil.doSendKeys(mobileNumber, "342423423");
+//            elementUtil.doSendKeys(country, "India");
+//            elementUtil.doSendKeys(firstCrush, "Hoor");
             //elementUtil.doClick(downloadLink);
 //            String text = driver.findElement(downloadLink).getText();
 //            System.out.println(text);
@@ -53,6 +54,7 @@ public class SelectorsHUBPractice {
 //        wait.until(ExpectedConditions.elementToBeClickable(submit))
 //        .click();
 
+/*
             By downloadLink = By.partialLinkText("DownLoad");
 
             WebElement link = driver.findElement(downloadLink);
@@ -75,11 +77,29 @@ public class SelectorsHUBPractice {
             System.out.println("Page not loaded");
         }
             Thread.sleep(3000);
+*/
 
-        driver.quit();
+
+            getUsersData();
+            driver.quit();
+        }
+
     }
+    public static void getUsersData ()
+        {
+            Actions act = new Actions(driver);
+            By userData = By.xpath("(//table[@class='table hover']/tbody/tr)");
+
+            List<WebElement> userTable = driver.findElements(userData);
+
+            for (int i = 0; i < userTable.size(); i++) {
+                WebElement row = userTable.get(i).findElement(By.xpath("./td[2]"));
+                act.scrollToElement(driver.findElement(By.xpath("//h6[text()='User Table']"))).build().perform();
+                String name = row.getText();
+                System.out.println(name);
+            }
+        }
 
 }
-
 
 
