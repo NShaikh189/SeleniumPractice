29466c35315a26b22705123d4d3a16cc8ce8ea7a
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-13 23:55:07 +0530

    Select user

--- src/test/java/org/example/SelectorsHUBPractice.java
+++ src/test/java/org/example/SelectorsHUBPractice.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class SelectorsHUBPractice {
     static WebDriver driver;
+    static WebDriverWait wait;
 
     public static void main(String[] args) throws InterruptedException {
         BrowserUtil browserUtil = new BrowserUtil();
@@ -24,32 +25,32 @@ public class SelectorsHUBPractice {
         browserUtil.launchUrl("https://selectorshub.com/xpath-practice-page/");
 
         if (elementUtil.isPageLoaded(3)) {
-//            By userEmail = By.name("email");
-//            By password = By.id("pass");
-//            By company = By.name("company");
-//            By mobileNumber = By.name("mobile number");
-//            By country = By.xpath("//label[text()='Country']/input");
-//            //  By submit = By.xpath("//button[@value = 'Submit']");
-//            By firstCrush = By.cssSelector("#inp_val");
-//            //By downloadLink = By.linkText("Download Link");
-//            //  By downloadLink = By.xpath("//*[@id='content']/div[2]/div/div[2]/div/div[1]/div[2]/div/div/span/a");
-//
-//            elementUtil.doSendKeys(userEmail, "abc@gmail.com");
-//            elementUtil.doSendKeys(password, "#1243@Test");
-//            elementUtil.doSendKeys(company, "CTS");
-//            elementUtil.doSendKeys(mobileNumber, "342423423");
-//            elementUtil.doSendKeys(country, "India");
-//            elementUtil.doSendKeys(firstCrush, "Hoor");
-            //elementUtil.doClick(downloadLink);
-//            String text = driver.findElement(downloadLink).getText();
-//            System.out.println(text);
-//            Actions act = new Actions(driver);
-//            act.scrollToElement(driver.findElement(downloadLink)).click().build().perform();
-//            System.out.println(browserUtil.getPageTitle());
-//        WebElement submitButton = driver.findElement(submit);
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
-//        wait.until(ExpectedConditions.elementToBeClickable(submit))
-//        .click();
+            By userEmail = By.name("email");
+            By password = By.id("pass");
+            By company = By.name("company");
+            By mobileNumber = By.name("mobile number");
+            By country = By.xpath("//label[text()='Country']/input");
+            //  By submit = By.xpath("//button[@value = 'Submit']");
+            By firstCrush = By.cssSelector("#inp_val");
+            //By downloadLink = By.linkText("Download Link");
+            //  By downloadLink = By.xpath("//*[@id='content']/div[2]/div/div[2]/div/div[1]/div[2]/div/div/span/a");
+
+            elementUtil.doSendKeys(userEmail, "abc@gmail.com");
+            elementUtil.doSendKeys(password, "#1243@Test");
+            elementUtil.doSendKeys(company, "CTS");
+            elementUtil.doSendKeys(mobileNumber, "342423423");
+            elementUtil.doSendKeys(country, "India");
+            elementUtil.doSendKeys(firstCrush, "Hoor");
+ /*           elementUtil.doClick(downloadLink);
+            String text = driver.findElement(downloadLink).getText();
+            System.out.println(text);
+            Actions act = new Actions(driver);
+            act.scrollToElement(driver.findElement(downloadLink)).click().build().perform();
+            System.out.println(browserUtil.getPageTitle());
+        WebElement submitButton = driver.findElement(submit);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.elementToBeClickable(submit))
+        .click();
 
 /*
             By downloadLink = By.partialLinkText("DownLoad");
@@ -78,14 +79,16 @@ public class SelectorsHUBPractice {
 
 
             getUsersData();
+
             driver.quit();
         }
 
+
     }
-    public static void getUsersData () throws InterruptedException {
+    public static void getUsersData () {
             Actions act = new Actions(driver);
             By userData = By.xpath("(//table[@class='table hover']/tbody/tr)");
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+            wait = new WebDriverWait(driver, Duration.ofSeconds(3));
             List<WebElement> userTable = wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElements(userData)));
 
             for (int i = 0; i < userTable.size(); i++) {
@@ -114,7 +117,6 @@ public class SelectorsHUBPractice {
 
                 }
 
-                Thread.sleep(2000);
             }
         }
 
